6ee2d473be82d0db94b8b154781e44bb610ddeaf
Richie <richimf1@gmail.com>
2017-02-17 10:43:54 -0600

    Update FuncionPuntero.c

--- FuncionPuntero.c
+++ FuncionPuntero.c
@@ -7,16 +7,15 @@ int (*miPunteroFuncion)(int,int);
 
 // Y una funcion cualquiera /o.o/
 int addInt(int n, int m){
-	return n+m;
+    return n+m;
 }
 
 int main(){
-	// Apuntamos a nuestra funcion   O.O!
-	// a nuestro puntero "miPunteroFuncion" le asignamos la referencia a nuestra funcion
-	miPunteroFuncion = &addInt; //"Puntero!, vigila a addInt()""
+    // Apuntamos a nuestra funcion   O.O!
+    // a nuestro puntero "miPunteroFuncion" le asignamos la referencia a nuestra funcion
+    miPunteroFuncion = &addInt; //"Puntero!, vigila a addInt()""
 
-	// Ahora usamos la funcion pueee /o.o/
-	int resultado = (*miPunteroFuncion)(3,5); // resultado = 8
-	printf(" resultado = %d \n",resultado);
-
+    // Ahora usamos la funcion pueee /o.o/
+    int resultado = (*miPunteroFuncion)(3,5); // resultado = 8
+    printf(" resultado = %d \n",resultado);
 }
